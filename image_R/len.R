9017c6fbfad5f68dc105fedfeae3f2fcb489d547
kate-plechova <kate.plechova@yandex.ru>
2021-06-09 13:01:38 +0300

    filter

--- image_R/len.R
+++ image_R/len.R
@@ -18,18 +18,9 @@ colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
 bed_df$len <- bed_df$end - bed_df$start
 head(bed_df)
 
-bed_df <- bed_df %>%
-  arrange(-len) %>%
-  filter(len < 50000)
-
 ggplot(bed_df) +
   aes(x = len) +
   geom_histogram() +
   ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
   theme_bw()
 ggsave(paste0('len_hist.', NAME, '.filtered.pdf'), path = OUT_DIR)
-
-bed_df %>%
-  select(-len) %>%
-  write.table(file='Giston/H3K27me3_SK-N-SH.ENCFF403PWJ.hg38.filtered.bed',
-              col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
